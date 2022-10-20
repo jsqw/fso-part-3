976c79883590ba2654785b0aa64728f65b31987e
juuso <108600998+jsqw@users.noreply.github.com>
2022-10-20 15:50:59 +0300

    phonebook getall

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+let persons = [
+  {
+    id: 1,
+    content: "James Maxwell",
+    number: "123456789",
+  },
+  {
+    id: 2,
+    content: "Leonardo Da Vinci",
+    number: "7562359",
+  },
+  {
+    id: 3,
+    content: "Mikael Kosola",
+    number: "22",
+  },
+];
+
+app.use(express.json());
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
