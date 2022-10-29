8272273a0c660277e28e546378229107f4139cf6
juuso <108600998+jsqw@users.noreply.github.com>
2022-10-30 01:28:06 +0300

    task 3_13

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const { response } = require("express");
+require("dotenv").config();
 const express = require("express");
+const Person = require("./models/person");
 const app = express();
-const PORT = process.env.PORT || 3001;
 const morgan = require("morgan");
 const cors = require("cors");
 app.use(cors());
 app.use(express.static("build"));
+app.use(express.json());
 
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
@@ -15,83 +16,23 @@ app.use(
   morgan(":method :url :status :res[name-length] - :response-time ms :body")
 );
 
-app.use(express.json());
-
-let persons = [
-  {
-    id: 1,
-    name: "James Maxwell",
-    number: "123456789",
-  },
-  {
-    id: 2,
-    name: "Leonardo Da Vinci",
-    number: "7562359",
-  },
-  {
-    id: 3,
-    name: "Mikael Kosola",
-    number: "22",
-  },
-];
-
-const generateId = () => {
-  return Math.floor(Math.random() * (1333337 - 2) + 2);
-};
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-app.get("/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
-});
-
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((people) => {
+    res.json(people);
+  });
 });
 
 app.get("/info", (req, res) => {
-  const date = new Date();
-  const infoSize = persons.length;
-  res.send(
-    `<p> Phonebook has information about ${infoSize} people <br> ${date} </p>`
-  );
+  Person.find({}).then((people) => {
+    const date = new Date();
+    const infoSize = people.length;
+    res.send(
+      `<p> Phonebook has information about ${infoSize} people <br> ${date} </p>`
+    );
+  });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((p) => p.id !== id);
-  res.status(204).end();
-});
-
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
-
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "Please enter a name and a number.",
-    });
-  }
-
-  if (persons.find((p) => p.name === body.name)) {
-    return res.status(400).json({
-      error: `${body.name} is already in the phonebook!`,
-    });
-  }
-
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  };
-
-  persons = persons.concat(person);
-  res.json(person);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
