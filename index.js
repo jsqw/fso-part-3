463e7a8005883ec892e4ed12980ef4d6dc2a1e9a
juuso <108600998+jsqw@users.noreply.github.com>
2022-10-31 00:14:52 +0200

    task 3_19

--- index.js
+++ index.js
@@ -52,20 +52,20 @@ app.get("/api/persons/:id", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
 
-  if (body.name === undefined || body.number === undefined) {
-    return res.status(400).json({ error: "Name and or number missing" });
-  }
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((p) => {
-    res.json(p);
-  });
+  person
+    .save()
+    .then((p) => {
+      res.json(p);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (req, res, next) => {
@@ -94,6 +94,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
