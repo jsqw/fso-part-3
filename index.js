0df53c9bccfd5e33b8cbb41d1bcf56eead197aed
juuso <108600998+jsqw@users.noreply.github.com>
2022-10-30 03:37:19 +0300

    task 3_17

--- index.js
+++ index.js
@@ -68,6 +68,21 @@ app.post("/api/persons", (req, res) => {
   });
 });
 
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
